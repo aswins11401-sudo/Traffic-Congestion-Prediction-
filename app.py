17ce0335596c2ac710ea4737b2577b18d43e99d7
Aswin <aswin.s11401@gmail.com>
2026-05-23 21:02:22 +0530

    Final Commit

--- app.py
+++ app.py
@@ -94,6 +94,11 @@ def predict(hour, day_of_week, direction, total_flow, avg_occupancy,
 
 st.title("🚦 Traffic Congestion Predictor")
 st.caption("Predicts average freeway speed using California PeMS D7 sensor data.")
+with st.sidebar:
+    st.title('INFO')
+    st.subheader(':green[Developed by] :red[Aswin]')
+    st.subheader(':red[LinkedIn :]www.linkedin.com/in/aswin-sgl')
+    st.subheader(':red[GitHub :]https://github.com/aswins11401-sudo')
 
 try:
     model, encoders, pt, skew_cols, feature_names = load_artifacts()
